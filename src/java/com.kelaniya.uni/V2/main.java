b219426270ea2770a7988dda717e15a0ee0b2a34
maheladissa <62756370+maheladissa@users.noreply.github.com>
2021-08-22 21:00:32 +0530

    V2 code final

--- src/java/com.kelaniya.uni/V2/main.java
+++ src/java/com.kelaniya.uni/V2/main.java
@@ -1,11 +1,16 @@
 package com.kelaniya.uni.V2;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         //Make sure to validate arguments
-        if (args.length==0) {
+        if (args.length == 0) {
             System.out.println("Please provide an argument");
             return;
         }
@@ -13,11 +18,27 @@ public class main {
 
         String operator = args[0];
 
-        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))){
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
             System.out.println("Please provide a valid argument");
             return;
         }
-        System.out.println(args[0]);
+        List<String> numbersStrs = Files.readAllLines(
+                Paths.get("C:/Users/Mahela/Documents/Software Construction/Calculator/src/numbers.txt")
+        );
+
+        double number1 = Double.parseDouble(numbersStrs.get(0));
+        double number2 = Double.parseDouble(numbersStrs.get(1));
+        double result = 0;
+
+        if (operator.equals("add")) {
+            result = number1 + number2;
+        } else if (operator.equals("sub")) {
+            result = number1 - number2;
+        } else if (operator.equals("mul")) {
+            result = number1 * number2;
+        }
+
+        System.out.println(result);
 
     }
 }
